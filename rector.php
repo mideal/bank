f1dee86b53392943445664706b95a18070540b90
Denis Mitrofanov <midealf@gmail.com>
2026-04-13 12:50:11 -0300

    pint

--- rector.php
+++ rector.php
@@ -8,7 +8,7 @@ return RectorConfig::configure()
         __DIR__.'/app',
         __DIR__.'/bootstrap',
         // __DIR__ . '/config',
-        __DIR__ . '/public',
+        __DIR__.'/public',
         __DIR__.'/resources',
         __DIR__.'/routes',
         __DIR__.'/tests',
